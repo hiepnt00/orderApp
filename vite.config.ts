6c2bc26ea2558592959132d762736608b59876f0
HiepNT <hiep.nt0000@gmail.com>
2025-12-10 19:58:53 +0700

    setup git page

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/orderApp/",
   plugins: [react()],
   server: {
-    port: 5173
-  }
-})
+    port: 5173,
+  },
+});
