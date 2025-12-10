8148c12d74b4be1cadb266a85d485039c019a05f
HiepNT <hiep.nt0000@gmail.com>
2025-12-10 20:17:49 +0700

    first commit

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -16,7 +16,7 @@ export default function MenuPage() {
   return (
     <div>
       <Typography variant="h5" gutterBottom>Menu</Typography>
-      123123123
+      123123123123123
       <Grid container spacing={2}>
         {items.map(item => (
           <Grid item xs={12} sm={6} md={4} key={item.id}>
